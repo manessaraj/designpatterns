971772a73458e5d89b4b51ae812dd92dbb9737ba
manesSaraj <manes.saraj@gmail.com>
2020-06-08 21:33:17 -0400

    Completing the Implementation of Bussiness Card client with Adapter Class with both way of adaption

--- src/adapter/Main.java
+++ src/adapter/Main.java
@@ -4,10 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		/** Using Class/Two-way adapter **/ 
-		
+
 		
 		/** Using Object Adapter **/
-		
+		Employee employee = new Employee();
+		EmployeeClassAdapter employeeClassAdapter = new EmployeeClassAdapter();
+		populateEmployeeData(employee);
+		populateEmployeeData(employeeClassAdapter);
+		EmployeeObjectAdapter employeeObjectAdapter = new EmployeeObjectAdapter(employee);
+		BusinessCardDesigner businessCardDesigner = new BusinessCardDesigner();
+		System.out.println(businessCardDesigner.designCard(employeeObjectAdapter));
+		System.out.println(businessCardDesigner.designCard(employeeClassAdapter));
+
+
 	}
 
 	private static void populateEmployeeData(Employee employee) {
